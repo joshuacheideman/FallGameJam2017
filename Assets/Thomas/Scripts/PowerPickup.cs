5b6b8c26b08798b5d5cacf1c3a1e128982eea35d
Thomas Tran <thomastran878@gmail.com>
2017-10-07 15:39:46 -0700

    Added scaling powerup

---
+++ Assets/Thomas/Scripts/PowerPickup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerPickup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.transform.parent.tag == "Player")
+        {
+            Debug.Log("PlayerEntered");
+        }
+    }
+}
---
+++ Assets/Thomas/Scripts/Scale.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scale : MonoBehaviour {
+    [SerializeField]
+    protected float scaleTime;
+    [SerializeField]
+    protected float scaleSize;
+
+    Vector3 startingScale;
+    Vector3 targetScale;
+
+	// Use this for initialization
+	void Start () {
+        startingScale = transform.localScale;
+        targetScale = new Vector3(scaleSize, scaleSize, scaleSize);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.G))
+        {
+            StartScaling();
+        }
+    }
+
+    public void StartScaling()
+    {
+        StartCoroutine(ScaleObject());
+    }
+
+    IEnumerator ScaleObject()
+    {
+        for (float t = 0; t < 1; t += Time.deltaTime / scaleSize)
+        {
+            transform.localScale = Vector3.Lerp(startingScale, targetScale, t);
+
+            yield return null;
+        }
+    }
+}
