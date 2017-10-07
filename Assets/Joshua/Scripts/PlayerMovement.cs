8395637d85fd822dbe489329818c940f527cb10c
Joshua Heideman <jcheideman@ucdavis.edu>
2017-10-07 13:32:19 -0700

    Made camera move with ball

--- Assets/Joshua/Scripts/PlayerMovement.cs
+++ Assets/Joshua/Scripts/PlayerMovement.cs
@@ -7,9 +7,14 @@ public class PlayerMovement : MonoBehaviour {
 	float movementHoriz;
 	float movementVert;
 	float speed = 5.0f;
+    Vector3 offset;
+	GameObject CurrentCam;
+	Transform CameraTransform;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+		CurrentCam = GameObject.FindGameObjectWithTag ("MainCamera");
+		offset = CurrentCam.transform.position - this.transform.position;
 	}
 	
 	// Update is called once per frame
@@ -17,5 +22,9 @@ public class PlayerMovement : MonoBehaviour {
 		movementHoriz = Input.GetAxis ("Horizontal");
 		movementVert = Input.GetAxis ("Vertical");
 		rb.AddForce(movementHoriz*speed,0.0f,movementVert*speed);
+	}
+	void LateUpdate()
+	{
+		CurrentCam.transform.position = this.transform.position + offset;
 	}
 }
