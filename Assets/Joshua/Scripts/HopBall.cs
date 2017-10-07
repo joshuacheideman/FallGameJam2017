1afc9248a0ddaf2080bab4b8d59d574a093b6efb
Joshua Heideman <jcheideman@ucdavis.edu>
2017-10-07 14:14:48 -0700

    Added Hop Functionality

---
+++ Assets/Joshua/Scripts/HopBall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HopBall : MonoBehaviour {
+
+	float JumpForce = 100.0f;
+	public void Hop(bool isGrounded,Rigidbody rb)
+	{
+		if (isGrounded && Input.GetButtonDown ("Jump")) {
+			Debug.Log ("Is Jumping");
+			rb.AddForce (0.0f, JumpForce, 0.0f);
+		}
+	}
+}
--- Assets/Joshua/Scripts/PlayerMovement.cs
+++ Assets/Joshua/Scripts/PlayerMovement.cs
@@ -4,27 +4,40 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 	Rigidbody rb;
+	MeshRenderer BallMesh;
 	float movementHoriz;
 	float movementVert;
+	float JumpForce;
 	float speed = 5.0f;
+	bool isGrounded=false;
     Vector3 offset;
 	GameObject CurrentCam;
-	Transform CameraTransform;
+	HopBall hop;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
 		CurrentCam = GameObject.FindGameObjectWithTag ("MainCamera");
 		offset = CurrentCam.transform.position - this.transform.position;
+		BallMesh = this.gameObject.GetComponent<MeshRenderer> ();
+		hop = GetComponent<HopBall> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		movementHoriz = Input.GetAxis ("Horizontal");
 		movementVert = Input.GetAxis ("Vertical");
+		if (BallMesh.material.color == Color.green) //checks if ball has a green material
+			hop.Hop (isGrounded,rb);
 		rb.AddForce(movementHoriz*speed,0.0f,movementVert*speed);
 	}
 	void LateUpdate()
 	{
 		CurrentCam.transform.position = this.transform.position + offset;
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "Floor")
+			isGrounded = true;
 	}
 }
