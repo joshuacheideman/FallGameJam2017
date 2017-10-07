c83f7433212845072893e023d2cd4ddc729481c7
Joshua Heideman <jcheideman@ucdavis.edu>
2017-10-07 13:14:39 -0700

    Ball Movement

---
+++ Assets/Joshua/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	Rigidbody rb;
+	float movementHoriz;
+	float movementVert;
+	float speed = 5.0f;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movementHoriz = Input.GetAxis ("Horizontal");
+		movementVert = Input.GetAxis ("Vertical");
+		rb.AddForce(movementHoriz*speed,0.0f,movementVert*speed);
+	}
+}
