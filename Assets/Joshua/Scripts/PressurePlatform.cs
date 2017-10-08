911ac3d5884178df0a85a65affa30ab78ea160f0
Joshua Heideman <jcheideman@ucdavis.edu>
2017-10-07 20:09:26 -0700

    Implemented Pressure Plates

---
+++ Assets/Joshua/Scripts/PressurePlatform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlatform : MonoBehaviour {
+	// Use this for initialization
+	public bool isPressed = false;
+	// Update is called once per frame
+	public bool GetSwitch()
+	{
+			return isPressed;
+	}
+	void OnCollisionEnter(Collision col)
+	{
+			if(col.collider.gameObject.tag =="Player")
+			{	
+				isPressed = true;
+			}	
+	}
+	void OnCollisionExit(Collision col)
+	{
+		if (col.collider.gameObject.tag == "Player") {
+			isPressed = false;
+		}
+	}
+
+}
