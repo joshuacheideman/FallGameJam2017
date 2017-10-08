af8a3683f63b657e8eaf6bbb2c5379463e18fa5f
dbelliss <daveybelliss@yahoo.com>
2017-10-07 17:53:40 -0700

    Adding Davey folder with NextLevel prefab and TestScene

---
+++ Assets/Davey/Scripts/NextLevelObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelObject : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("Going to next level");
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
