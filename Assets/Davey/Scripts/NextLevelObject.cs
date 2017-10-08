825e629eb8d68f9b0fd79a9394afa53a9cc1852e
dbelliss <daveybelliss@yahoo.com>
2017-10-07 21:45:24 -0700

    Adding main menu scene and updating playerUI prefab and flag script

--- Assets/Davey/Scripts/NextLevelObject.cs
+++ Assets/Davey/Scripts/NextLevelObject.cs
@@ -13,8 +13,16 @@ public class NextLevelObject : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
 		Debug.Log ("Going to next level");
 		completionSound.Play ();
+		StartCoroutine (nextLevel ());
+
+	}
+
+	IEnumerator nextLevel() {
 		GameObject.FindGameObjectWithTag ("Timer").GetComponent<Timer> ().stopTimer ();
 		GameObject.FindGameObjectWithTag ("BombTimer").GetComponent<BombTimer> ().enabled = false;
 
+		yield return new WaitForSeconds (3f);
+
+		gameManager.GetComponent<GameManager> ().LoadNextScene ();
 	}
 }
